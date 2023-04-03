3274fda71b101463ea832852318c12c5c8aebcdc
Mohammad Momenuzzaman <75083550+nosferatu0205@users.noreply.github.com>
2023-04-03 11:53:38 +0600

    Update PlaintextToHtmlConverter.java

--- java/src/main/java/workshop/PlaintextToHtmlConverter.java
+++ java/src/main/java/workshop/PlaintextToHtmlConverter.java
@@ -2,43 +2,75 @@ package workshop;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlaintextToHtmlConverter {
-    private static final String INPUT_FILE_PATH = "sample.txt";
-    private static final String NEW_LINE_SEPARATOR = "<br />";
-    private static final String HTML_ENCODING_LT = "&lt;";
-    private static final String HTML_ENCODING_GT = "&gt;";
-    private static final String HTML_ENCODING_AMP = "&amp;";
 
-    public String toHtml() {
-        String plainText;
-        try {
-            plainText = readInputFile();
-        } catch (IOException e) {
-            
-            return "";
-        }
+    private String source;
+    private int i;
+    private List<String> result;
+    private List<String> convertedLine;
+    private String characterToConvert;
 
-        StringBuilder html = new StringBuilder();
-        for (int i = 0; i < plainText.length(); i++) {
-            char c = plainText.charAt(i);
-            if (c == '<') {
-                html.append(HTML_ENCODING_LT);
-            } else if (c == '>') {
-                html.append(HTML_ENCODING_GT);
-            } else if (c == '&') {
-                html.append(HTML_ENCODING_AMP);
-            } else if (c == '\n') {
-                html.append(NEW_LINE_SEPARATOR);
-            } else {
-                html.append(c);
-            }
-        }
-        return html.toString();
+    public String convertToHtml(Path filePath) throws IOException {
+        String text = read(filePath);
+        String htmlLines = basicHtmlEncode(text);
+        return htmlLines;
     }
 
-    private String readInputFile() throws IOException {
-        return new String(Files.readAllBytes(Paths.get(INPUT_FILE_PATH)));
+    private String read(Path filePath) throws IOException {
+        byte[] fileBytes = Files.readAllBytes(filePath);
+        return new String(fileBytes);
+    }
+
+    private String basicHtmlEncode(String source) {
+        this.source = source;
+        i = 0;
+        result = new ArrayList<>();
+        convertedLine = new ArrayList<>();
+        characterToConvert = getNextCharacter();
+
+        while (characterToConvert != null) {
+            switch (characterToConvert) {
+                case "<":
+                    convertedLine.add("&lt;");
+                    break;
+                case ">":
+                    convertedLine.add("&gt;");
+                    break;
+                case "&":
+                    convertedLine.add("&amp;");
+                    break;
+                case "\n":
+                    addNewLine();
+                    break;
+                default:
+                    pushCharacterToOutput();
+            }
+            characterToConvert = getNextCharacter();
+        }
+        addNewLine();
+        return String.join("<br />", result);
+    }
+
+    private String getNextCharacter() {
+        if (i < source.length()) {
+            String nextChar = String.valueOf(source.charAt(i));
+            i++;
+            return nextChar;
+        }
+        return null;
+    }
+
+    private void addNewLine() {
+        String line = String.join("", convertedLine);
+        result.add(line);
+        convertedLine.clear();
+    }
+
+    private void pushCharacterToOutput() {
+        convertedLine.add(characterToConvert);
     }
 }
